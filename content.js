5be00c619b9d2f231ac4e35a53f83583ce046d22
yohanstra <jreinstra@gmail.com>
2017-09-09 19:18:28 -0400

    Detect link press

--- content.js
+++ content.js
@@ -6,9 +6,11 @@ var url = window.location.href;
 var html = '<div style="width: 50% !important; height:100%;"> <iframe id="left" style="width: 100%; height: 100%;" src="' + url + '" onload="didLoadFrame()"></iframe></div> <div style="width: 50% !important;"></div>';
 
 
-document.getElementById("left").onload = function () {
+document.write(html);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  	document.getElementById("left").onload = function () {
 		a = document.getElementById("left").contentDocument.getElementsByTagName('a');
-	alert(a.length);
 	for (var i = 0; i < a.length; i++) {
 	    a[i].onclick = function () {
 	    	alert("hello");
@@ -17,8 +19,9 @@ document.getElementById("left").onload = function () {
 	    };
 	};
 }
+});
 
 
-document.write(html);
 document.close();
+
 
