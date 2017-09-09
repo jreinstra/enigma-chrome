49e982c9617cddf6ee3bcc98cb55289969a5e686
yohanstra <jreinstra@gmail.com>
2017-09-09 15:20:39 -0400

    meh

--- content.js
+++ content.js
@@ -2,6 +2,8 @@
 
 //alert(document.getElementsByTagName("title")[0].innerText);
 
-var inner = document.getElementsByTagName("body")[0].innerHTML;
-console.log("inner:", inner);
-document.getElementsByTagName("body")[0].innerHTML = '<div style="width: 50% !important;">' inner + '</div>' + '<div style="width: 50%;"></div>';
+setTimeout(function() {
+	var inner = document.getElementsByTagName("body")[0].innerHTML;
+	console.log("inner:", inner);
+	document.getElementsByTagName("body")[0].innerHTML = '<div style="width: 50% !important;">' + inner + '</div>' + '<div style="width: 50%;"></div>';
+}, 10000);
