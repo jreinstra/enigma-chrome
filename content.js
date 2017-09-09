b50502ba5035cc02f1fef249d6ebb443fa941540
yohanstra <jreinstra@gmail.com>
2017-09-09 13:02:25 -0400

    start

---
+++ content.js
@@ -0,0 +1,7 @@
+//alert("im pickle rick");
+
+//alert(document.getElementsByTagName("title")[0].innerText);
+
+var inner = document.getElementsByTagName("body")[0].innerHTML;
+console.log("inner:", inner);
+document.getElementsByTagName("body")[0].innerHTML = '<div style="width: 50% !important;">' inner + '</div>' + '<div style="width: 50%;"></div>';
