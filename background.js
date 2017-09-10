3c5e70247040ec20837b330a26260e8b93a0a2da
Jason Scharff <jasonscharff@gmail.com>
2017-09-10 02:35:12 -0400

    move to new system

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    for (var i = 0; i < details.responseHeaders.length; ++i) {
-        var lowercaseHeaderName = details.responseHeaders[i].name.toLowerCase();
-        
-      if (lowercaseHeaderName== 'x-frame-options') {
-        details.responseHeaders.splice(i, 1);
-      } else if (details.responseHeaders[i].name.toLowerCase() == 'content-security-policy') {
-     //     details.responseHeaders.splice(i, 1);
-      }
-        return {
-          responseHeaders: details.responseHeaders
-        };
-    }
-
-  }, {
-    urls: ["https://*.vox.com/*", "https://*.huffingtonpost.com/*", "http://*.breitbart.com/*", "https://*.theblaze.com/*", "https://*.foxnews.com/*", "https://*.cnn.com/*", "https://*.wsj.com/*", "https://*.realclearpolitics.com/*", "https://*.forbes.com/*", "https://*.nbcnews.com/*", "https://*.cbsnews.com/*", "https://*.bloomberg.com/*", "https://*.politico.com/*", "https://*.thewashingtonpost.com/*", "https://*.nytimes.com/*", "https://*.nypost.com/*", "https://*.bluelivesmatter.blue/*"]
-  }, ["blocking", "responseHeaders"]);
+    function(info) {
+        var headers = info.responseHeaders;
+        for (var i=headers.length-1; i>=0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options') {
+                headers.splice(i, 1); // Remove header
+            }
+        }
+        return {responseHeaders: headers};
+    },
+    {
+        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
+        types: [ 'sub_frame' ]
+    },
+    ['blocking', 'responseHeaders']
+);
--- content.js
+++ content.js
@@ -1,43 +1,25 @@
-document.open("text/html", "replace");
 
-function updateUrl(url, headline) {
-    console.log(url, headline);
-    var success_func = function(data) {
-        console.log(data);
-        var url = data.url;
-        if(url === false) url = "https://example.com/";
-        $("#right").attr("src", url.replace("http://", "https://"));
-    }
-    $.ajax("https://enigma.joseb.me/analyze", {
-        "data": JSON.stringify({"site": url, "headline": headline}),
-        "type": "POST",
-        "processData": false,
-        "success": success_func,
-        "contentType": "application/json"
-    });
-}
-
-
-var url = window.location.href;
-var html = '<html><head></head><body><div style="width: 49.7% !important; display:inline-block;"> <iframe id="left" style="width: 100%; height: 100%;" src="' + url + '"></iframe></div> <div style="width: 49.7% !important; display:inline-block;"><iframe id="right" style="width: 100%; height: 100%;"></iframe></div></body></html>';
-
-
-document.write(html);
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    document.getElementById("left").onload = function () {
-        var contentDoc = document.getElementById("left").contentDocument;
-        updateUrl(window.location.href, contentDoc.getElementsByTagName("title")[0].innerText);
-        a = contentDoc.getElementsByTagName('a');
-        for (var i = 0; i < a.length; i++) {
-            a[i].onclick = function (e) {
-                console.log(e, e.parent);
-                updateUrl(e.target.href, e.target.innerText);
-                return true;
-            };
-        };
+    
+    var success_func = function(data) {
+    console.log(data);
+    var url = data.url;
+    if(url != false) {
+        window.location.href = "https://enigma.joseb.me/view"
+    }
 }
+
+    
+if (window.location.pathname != "/") {
+    $.ajax("https://enigma.joseb.me/analyze", {
+    "data": JSON.stringify({"site": window.location.href, "headline": document.getElementsByTagName("title")[0].innerText}),
+    "type": "POST",
+    "processData": false,
+    "success": success_func,
+    "contentType": "application/json"
 });
+}
+    
 
-
-document.close();
+});
