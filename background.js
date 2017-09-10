0579e45d65a30292c293108ea5d92408c443428c
Jason Scharff <jasonscharff@gmail.com>
2017-09-10 04:01:37 -0400

    Prepare for button

--- background.js
+++ background.js
@@ -1,17 +1,54 @@
 chrome.webRequest.onHeadersReceived.addListener(
     function(info) {
         var headers = info.responseHeaders;
-        for (var i=headers.length-1; i>=0; --i) {
+        for (var i = headers.length - 1; i >= 0; --i) {
             var header = headers[i].name.toLowerCase();
             if (header == 'x-frame-options' || header == 'frame-options') {
                 headers.splice(i, 1); // Remove header
             }
         }
-        return {responseHeaders: headers};
-    },
-    {
-        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
-        types: [ 'sub_frame' ]
-    },
-    ['blocking', 'responseHeaders']
+        return {
+            responseHeaders: headers
+        };
+    }, {
+        urls: ['*://*/*'], // Pattern to match all http(s) pages
+        types: ['sub_frame']
+    }, ['blocking', 'responseHeaders']
 );
+
+chrome.browserAction.onClicked.addListener(
+    function(tab) {
+
+            var success_func = function(data) {
+                alert("test");
+                console.log(data);
+                var url = data.url;
+                if (url != false) {
+                    window.location.href = "https://enigma.joseb.me/view";
+                } else {
+                    alert("No articles could be found.");
+                }
+            };
+            
+            var error_func = function(data) {
+                alert(data);
+            };
+            
+
+
+            if (window.location.pathname != "/") {
+                $.ajax("https://enigma.joseb.me/analyze", {
+                    "data": JSON.stringify({
+                        "site": window.location.href,
+                        "headline": document.getElementsByTagName("title")[0].innerText
+                    }),
+                    "type": "POST",
+                    "processData": false,
+                    "success": success_func,
+                    "error" : error_func,
+                    "contentType": "application/json"
+                });
+            }
+
+
+    });
